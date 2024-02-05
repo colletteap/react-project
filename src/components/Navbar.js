5b175ec85a5a01c0b1b72b600269dc907f43ae71
colletteap <garlandcollette@gmail.com>
2024-02-05 12:44:57 -0330

    just getting react started

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import Logo from '../assets/logo-black.png';
+
+function Navbar() {
+  return (
+    <div className="navbar">Navbar
+    <div className="leftSide">
+        <img src={Logo} />
+    </div>
+    <div className="rightSide"></div>
+    </div>
+  )
+}
+
+export default Navbar
