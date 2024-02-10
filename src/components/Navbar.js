8c952465c2501e3b52e2bf20f450c00699b5f1d6
colletteap <garlandcollette@gmail.com>
2024-02-10 14:10:27 -0330

    added pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,32 @@
+import React from "react";
 import Logo from '../assets/logo-black.png';
+import { Link } from "react-router-dom";
+import ReorderIcon from '@mui/icons-material/Reorder';
+import '../styles/Navbar.css';
 
 function Navbar() {
   return (
     <div className="navbar">Navbar
     <div className="leftSide">
         <img src={Logo} />
+        <div className="hiddenLinks">
+        <Link to="./"> Home </Link>
+      <Link to="./Profile"> Profile </Link>
+      <Link to="./SkillShell"> Skill Shell </Link>
+      <Link to="./Advice"> Advice </Link>
+      <Link to="./Blog"> Blog </Link>
+        </div>
     </div>
-    <div className="rightSide"></div>
+    <div className="rightSide">
+      <Link to="./"> Home </Link>
+      <Link to="./Profile"> Profile </Link>
+      <Link to="./SkillShell"> Skill Shell </Link>
+      <Link to="./Advice"> Advice </Link>
+      <Link to="./Blog"> Blog </Link>
+      <button>
+        <ReorderIcon />
+      </button>
+    </div>
     </div>
   )
 }
