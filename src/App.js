016c33a2efefd45328e3579a51129e20debfdf85
Collette <garlandcollette@gmail.com>
2024-02-22 16:44:19 -0330

    trouble shooting click errors

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import SkillShell from "./pages/SkillShell";
 import Profile from "./pages/Profile";
 import Advice from "./pages/Advice";
 import Blog from "./pages/Blog";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
       <Navbar />
-      <Switch>
+      <Routes>
         <Route path="/" exact component={Home}/>
         <Route path="/SkillShell" exact component={SkillShell}/>
         <Route path="/Profile" exact component={Profile}/>
         <Route path="/Advice" exact component={Advice}/>
         <Route path="/Blog" exact component={Blog}/>
-      </Switch>
+      </Routes>
       </Router>
     
        </div>
--- src/pages/SkillShell.js
+++ src/pages/SkillShell.js
@@ -3,6 +3,7 @@ import React from 'react'
 function SkillShell() {
   return (
     <div>Skill Shell</div>
+    {onClick={() => console.log ('Clicked')}}
   )
 }
 
