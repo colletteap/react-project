cb7d95b64edff373b78fdd647bb0d263950c3639
Collette <garlandcollette@gmail.com>
2024-03-09 13:48:32 -0330

    added links to cards

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -11,7 +11,7 @@ function Blog() {
       <Blogcard
         title="Google Classroom Tips"
         content="NewEd Tech was a YouTube channel geared toward helping teachers learn tech is simple, quick & manageable bites. They then expanded into a website where, with a free plan, you can learn bite sized, useful teacher skills."
-        linkTo="/Blog.js"
+        linkTo="https://newedtechclassroom.com/"
         image={BlogLanding1}
         para1=""
         para2=""
@@ -19,7 +19,7 @@ function Blog() {
       <Blogcard
         title="Brene Brown's Leadership Tools"
         content="Brene Brown has a Ph. D in the social work field, has a very popular Ted talk and has a Netflix special based around personal development so that you can bring your best self into every situation."
-        linkTo="/Blog.js"
+        linkTo="https://brenebrown.com/hubs/dare-to-lead/"
         image={BlogLanding2}
         para1=""
         para2=""
@@ -27,7 +27,7 @@ function Blog() {
       <Blogcard
         title="The Happy Teacher Planner"
         content="With this planner, you can use a personalized calendar to keep track of special events and your to do list; focus on your personal growth through using a reflective journal and core values list; has a substitute plan and has resources for the tough times. "
-        linkTo="/Blog.js"
+        linkTo="https://colletteap.github.io/gratitude-planner/"
         image={BlogLanding3}
         para1=""
         para2=""
