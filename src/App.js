f061f372c95d2331e1c119b9dafc57bbf5d44deb
Collette <garlandcollette@gmail.com>
2024-03-17 12:08:58 -0230

    remove an import from app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
 import SkillShell from "./pages/SkillShell";
 import Advice from "./pages/Advice";
 import Blog from "./pages/Blog";
