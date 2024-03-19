67a0226d60826bd7fb6680a0b3c9ce098f89fb06
Collette <garlandcollette@gmail.com>
2024-03-19 20:09:20 -0230

    created extra pages on skillshell

--- src/components/Skilllanding.js
+++ src/components/Skilllanding.js
@@ -7,23 +7,23 @@ const SkillLanding = () => {
     <>
       <h1 className="whiteHeading">Skills from real teachers</h1>
       <div className="SkillContainer">
-        <SkillButton skill="Technology" linkTo="/SkillShell" />
-        <SkillButton skill="Classroom Management" linkTo="/SkillShell" />
-        <SkillButton skill="Life Work Balance" linkTo="/SkillShell" />
-        <SkillButton skill="Art" linkTo="/SkillShell" />
-        <SkillButton skill="Home Economics" linkTo="/SkillShell" />
-        <SkillButton skill="Boundary Setting" linkTo="/SkillShell" />
-        <SkillButton skill="Time Management" linkTo="/SkillShell" />
-        <SkillButton skill="Organization" linkTo="/SkillShell" />
-        <SkillButton skill="Physical Education" linkTo="/SkillShell" />
-        <SkillButton skill="Conflict Management" linkTo="/SkillShell" />
-        <SkillButton skill="Soft Skills" linkTo="/SkillShell" />
-        <SkillButton skill="Behaviour Management" linkTo="/SkillShell" />
-        <SkillButton skill="Powerschool" linkTo="/SkillShell" />
-        <SkillButton skill="Resourcefulness" linkTo="/SkillShell" />
-        <SkillButton skill="Critical Thinking" linkTo="/SkillShell" />
-        <SkillButton skill="Leadership" linkTo="/SkillShell" />
-        <SkillButton skill="Creativity" linkTo="/SkillShell" />
+        <SkillButton skill="Technology" linkTo="/SkillShell/Technology" />
+        <SkillButton skill="Classroom Management" linkTo="/SkillShell/Classroom-Management" />
+        <SkillButton skill="Life Work Balance" linkTo="/SkillShell/Life-Work-Balance" />
+        <SkillButton skill="Art" linkTo="/SkillShell/Art" />
+        <SkillButton skill="Home Economics" linkTo="/SkillShell/Home-Economics" />
+        <SkillButton skill="Boundary Setting" linkTo="/SkillShell/Boundary-Setting" />
+        <SkillButton skill="Time Management" linkTo="/SkillShell/Time-Management" />
+        <SkillButton skill="Organization" linkTo="/SkillShell/Organization" />
+        <SkillButton skill="Physical Education" linkTo="/SkillShell/Physical-Education" />
+        <SkillButton skill="Conflict Management" linkTo="/SkillShell/Conflict-Management" />
+        <SkillButton skill="Soft Skills" linkTo="/SkillShell/Soft-Skills" />
+        <SkillButton skill="Behaviour Management" linkTo="/SkillShell/Behaviour-Management" />
+        <SkillButton skill="Powerschool" linkTo="/SkillShell/Powerschool" />
+        <SkillButton skill="Resourcefulness" linkTo="/SkillShell/Resourcefulness" />
+        <SkillButton skill="Critical Thinking" linkTo="/SkillShell/Critical-Thinking" />
+        <SkillButton skill="Leadership" linkTo="/SkillShell/Leadership" />
+        <SkillButton skill="Creativity" linkTo="/SkillShell/Creativity" />
         <SkillButton skill="More+" linkTo="/SkillShell" />
       </div>
     </>
