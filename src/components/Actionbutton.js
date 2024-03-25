976f549bcbc59ebf78b3cd0c95800616da8b2091
Collette <garlandcollette@gmail.com>
2024-03-25 12:33:16 -0230

    action button update

--- src/components/Actionbutton.js
+++ src/components/Actionbutton.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
 import { Typography } from "@mui/material";
-import PostCard from "./Postcard"; 
 
 
 export default function ActionButton({ skill, onClick }) {
