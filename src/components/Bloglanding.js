be5d7bb7e9ab25c0227d97c3c259452197fba872
Collette <garlandcollette@gmail.com>
2024-03-25 13:40:46 -0230

    fixed broken link on Bloglanding within Gridlanding

--- src/components/Bloglanding.js
+++ src/components/Bloglanding.js
@@ -5,7 +5,7 @@ import BlogLanding1 from "../assets/bloglanding1.png";
 import BlogLanding2 from "../assets/bloglanding2.png";
 import BlogLanding3 from "../assets/bloglanding3.png";
 
-const AdviceLanding = () => {
+const BlogLanding = () => {
   return (
     <>
       <h1 className="whiteHeading">Resources from real teachers</h1>
@@ -13,19 +13,19 @@ const AdviceLanding = () => {
         <Landingblogcard
           title="Google Classroom Tips"
           content="NewEd Tech was a YouTube channel geared toward helping teachers learn tech is simple, quick & manageable bites. They then expanded into a website where, with a free plan, you can learn bite sized, useful teacher skills."
-          linkTo="/Blog.js"
+          linkTo="/Blog"
           image={BlogLanding1}
         />
         <Landingblogcard
           title="Brene Brown's Leadership Tools"
           content="Brene Brown has a Ph. D in the social work field, has a very popular Ted talk and has a Netflix special based around personal development so that you can bring your best self into every situation."
-          linkTo="/Blog.js"
+          linkTo="/Blog"
           image={BlogLanding2}
         />
         <Landingblogcard
           title="The Happy Teacher Planner"
           content="With this planner, you can use a personalized calendar to keep track of special events and your to do list; focus on your personal growth through using a reflective journal and core values list; has a substitute plan and has resources for the tough times. "
-          linkTo="/Blog.js"
+          linkTo="/Blog"
           image={BlogLanding3}
         />
       </div>
@@ -33,4 +33,4 @@ const AdviceLanding = () => {
   );
 };
 
-export default AdviceLanding;
+export default BlogLanding;
