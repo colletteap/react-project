16b4a2176796a0f028a8d3cd1b9e086060217335
Collette <garlandcollette@gmail.com>
2024-03-26 08:50:26 -0230

    made skillbutton into an array for skilllanding

--- src/components/Skilllanding.js
+++ src/components/Skilllanding.js
@@ -2,6 +2,26 @@ import React from "react";
 import "../styles/Skilllanding.css";
 import SkillButton from "./Skillbutton";
 
+const skills = [
+  { skill: "Technology", linkTo: "/SkillShell/Technology" },
+  { skill: "Classroom Management", linkTo: "/SkillShell/Classroom-Management" },
+  { skill: "Life Work Balance", linkTo: "/SkillShell/Life-Work-Balance" },
+  { skill: "Art", linkTo: "/SkillShell/Art" },
+  { skill: "Home Economics", linkTo: "/SkillShell/Home-Economics" },
+  { skill: "Boundary Setting", linkTo: "/SkillShell/Boundary-Setting" },
+  { skill: "Time Management", linkTo: "/SkillShell/Time-Management" },
+  { skill: "Organization", linkTo: "/SkillShell/Organization" },
+  { skill: "Physical Education", linkTo: "/SkillShell/Physical-Education" },
+  { skill: "Conflict Management", linkTo: "/SkillShell/Conflict-Management" },
+  { skill: "Soft Skills", linkTo: "/SkillShell/Soft-Skills" },
+  { skill: "Behaviour Management", linkTo: "/SkillShell/Behaviour-Management" },
+  { skill: "Powerschool", linkTo: "/SkillShell/Powerschool" },
+  { skill: "Resourcefulness", linkTo: "/SkillShell/Resourcefulness" },
+  { skill: "Critical Thinking", linkTo: "/SkillShell/Critical-Thinking" },
+  { skill: "Leadership", linkTo: "/SkillShell/Leadership" },
+  { skill: "Creativity", linkTo: "/SkillShell/Creativity" },
+  { skill: "More+", linkTo: "/SkillShell" }
+];
 
 const SkillLanding = () => {
 
@@ -9,57 +29,8 @@ const SkillLanding = () => {
     <>
       <h1 className="whiteHeading">Skills from real teachers</h1>
       <div className="SkillContainer">
-        <SkillButton
-          skill="Technology"
-          linkTo="/SkillShell/Technology"
-        />
-        <SkillButton
-          skill="Classroom Management"
-          linkTo="/SkillShell/Classroom-Management"
-        />
-        <SkillButton
-          skill="Life Work Balance"
-          linkTo="/SkillShell/Life-Work-Balance"
-        />
-        <SkillButton skill="Art" linkTo="/SkillShell/Art" />
-        <SkillButton
-          skill="Home Economics"
-          linkTo="/SkillShell/Home-Economics"
-        />
-        <SkillButton
-          skill="Boundary Setting"
-          linkTo="/SkillShell/Boundary-Setting"
-        />
-        <SkillButton
-          skill="Time Management"
-          linkTo="/SkillShell/Time-Management"
-        />
-        <SkillButton skill="Organization" linkTo="/SkillShell/Organization" />
-        <SkillButton
-          skill="Physical Education"
-          linkTo="/SkillShell/Physical-Education"
-        />
-        <SkillButton
-          skill="Conflict Management"
-          linkTo="/SkillShell/Conflict-Management"
-        />
-        <SkillButton skill="Soft Skills" linkTo="/SkillShell/Soft-Skills" />
-        <SkillButton
-          skill="Behaviour Management"
-          linkTo="/SkillShell/Behaviour-Management"
-        />
-        <SkillButton skill="Powerschool" linkTo="/SkillShell/Powerschool" />
-        <SkillButton
-          skill="Resourcefulness"
-          linkTo="/SkillShell/Resourcefulness"
-        />
-        <SkillButton
-          skill="Critical Thinking"
-          linkTo="/SkillShell/Critical-Thinking"
-        />
-        <SkillButton skill="Leadership" linkTo="/SkillShell/Leadership" />
-        <SkillButton skill="Creativity" linkTo="/SkillShell/Creativity" />
-        <SkillButton skill="More+" linkTo="/SkillShell" />
+        {skills.map((skill, index) => (<SkillButton key={index} skill={skill.skill} linkTo={skill.linkTo} />
+        ))}
       </div>
     </>
   );
