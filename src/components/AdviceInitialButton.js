6d7774d7cb7163846cacf10f8053760efde3b25c
Collette <garlandcollette@gmail.com>
2024-03-30 10:34:11 -0230

    working on Advice page - initial buttons

---
+++ src/components/AdviceInitialButton.js
@@ -0,0 +1,37 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
+
+
+export default function AdviceInitialButton({ type, onClick }) {
+
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
+        gridGap: "10px",
+        padding: "10px",
+        justifyItems: "center",
+      }}
+    >
+        <Button
+          variant="contained"
+          size="large"
+          sx={{ bgcolor: "#233349", color: "#ffff", fontWeight: "bolder", borderRadius: "10px", "&:hover": {backgroundColor: "#5378AB",} }}
+          onClick={onClick}
+        >
+          <Typography
+            sx={{
+              fontFamily: "Verdana",
+              fontSize: "40px",
+              textTransform: "initial",
+              whiteSpace: "nowrap",
+            }}
+          >
+            {type}
+          </Typography>
+        </Button>
+    </div>
+  );
+}
--- src/pages/Advice.js
+++ src/pages/Advice.js
@@ -4,9 +4,20 @@ import ActionButton from "../components/Actionbutton";
 import QuestionCard from "../components/Questioncard";
 import PostCard from "../components/Postcard";
 import SearchBar from "../components/Searchbar";
+import AdviceInitialButton from "../components/AdviceInitialButton";
 
 function Advice() {
   const [showPostcard, setShowPostcard] = useState(false);
+  const [showReceiveAdvice, setShowReceiveAdvice] = useState(false);
+  const [showGiveAdvice, setShowGiveAdvice] = useState(false);
+
+  const handleReceiveAdviceClick = () => {
+    setShowReceiveAdvice(true);
+  };
+
+  const handleGiveAdviceClick = () => {
+    setShowGiveAdvice(true);
+  };
 
   const handleBtnAClick = () => {
     console.log("show postcard", showPostcard)
@@ -19,22 +30,27 @@ function Advice() {
 
   return (
     <div>
-      <div className="AdviceButtonContainer">
-        <ActionButton skill="Ask Question" onClick={handleBtnAClick} />
-        <ActionButton skill="Add Advice"  onClick={handleBtnAClick} />
-      </div>
-      <div className="AdviceInputContainer">
-        {showPostcard && <PostCard />}
-        </div>
       <div className="SearchBarContainer">
         <SearchBar />
       </div>
-      <div className="Titles">
-        <h2>Advice</h2>
-        <h2>Questions</h2>
+      <div className="AdviceButtonContainer">
+        <AdviceInitialButton type="Receive Advice" onClick={handleReceiveAdviceClick} />
+        <AdviceInitialButton type="Ask For Advice" onClick={handleGiveAdviceClick} />
       </div>
-      <div className="AdvicePostsContainer">
-        <div className="InsideAdvicePostsContainer">
+     
+      <div className="AdviceInputContainer">
+        {showPostcard && <PostCard />}
+        </div>
+    
+      
+      {showReceiveAdvice && (
+        <>
+        <h2 className="Titles">Receive Advice</h2>
+        <div className="AdviceButtonContainer">
+        <ActionButton skill="Ask Question" onClick={handleBtnAClick} />
+       
+      </div>
+        <div className="receiveAdviceDiv">
           <QuestionCard cardID="1" />
           <QuestionCard cardID="2" />
           <QuestionCard cardID="1" />
@@ -42,8 +58,16 @@ function Advice() {
           <QuestionCard cardID="1" />
           <QuestionCard cardID="2" />
         </div>
-
-        <div className="InsideAdvicePostsContainer">
+        </>
+      )}
+      {showGiveAdvice && (
+        <>
+        <h2 className="Titles">Give Advice</h2>
+        <div className="AdviceButtonContainer">
+        
+        <ActionButton skill="Add Advice"  onClick={handleBtnAClick} />
+      </div>
+        <div className="giveAdviceDiv">
           <QuestionCard cardID="1" />
           <QuestionCard cardID="2" />
           <QuestionCard cardID="1" />
@@ -51,8 +75,11 @@ function Advice() {
           <QuestionCard cardID="1" />
           <QuestionCard cardID="2" />
         </div>
+      
+      </>
+      )}
       </div>
-    </div>
+  
   );
 }
 
