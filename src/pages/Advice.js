eda6f56d208ebc1e4120af8bf7d98cc0415b4a97
Collette <garlandcollette@gmail.com>
2024-04-03 18:02:30 -0230

    fixed Advice page styling

--- src/pages/Advice.js
+++ src/pages/Advice.js
@@ -5,87 +5,54 @@ import ActionButton from "../components/Actionbutton";
 import ContentCard from "../components/Contentcard";
 import PostCard from "../components/Postcard";
 import SearchBar from "../components/Searchbar";
-import AdviceInitialButton from "../components/AdviceInitialButton";
+import Home from "../assets/Home.png";
+import AdviceText from "../assets/AdviceText.png";
 import GiveAdvice from "../assets/GiveAdvice.png";
 import AskAdvice from "../assets/AskAdvice.png";
-import Home from "../assets/Home.png";
-import Back from "../assets/Back.png";
-import { askAdviceCardData, addCommentToArray } from "../components/Data";
+import { askAdviceCardData, addCommentToArray, } from "../components/Data";
 
 function Advice() {
   const [showPostcard, setShowPostcard] = useState(false);
-  const [showAskAdvice, setShowAskAdvice] = useState(false);
-  const [showGiveAdvice, setShowGiveAdvice] = useState(false);
-  const [showAdviceTopContainer, setShowAdviceTopContainer] = useState(true);
-  const [showBackButton, setShowBackButton] = useState(false);
+  const [showAskQuestion, setShowAskQuestion] = useState(true);
   const [commentsArray, setCommentsArray] = useState(() => {
     const storedComments = localStorage.getItem('commentsArray');
     return storedComments ? JSON.parse(storedComments) : [];
   });
 
-  const handleAskAdviceClick = () => {
-    setShowAskAdvice(true);
-    setShowAdviceTopContainer(false);
-    setShowBackButton(true);
-  };
-
-  const handleGiveAdviceClick = () => {
-    setShowGiveAdvice(true);
-    setShowAdviceTopContainer(false);
-    setShowBackButton(true);
-  };
 
   const handleBtnAClick = () => {
     console.log("show postcard", showPostcard);
     if (!showPostcard) {
       setShowPostcard(true);
-    } else {
-      console.log("Postcard is already open");
-    }
+      setShowAskQuestion(false);
+    } 
   };
 
-  const handleBackButton = () => {
-    setShowAskAdvice(false);
-    setShowGiveAdvice(false);
-    setShowAdviceTopContainer(true);
-    setShowBackButton(false);
-    setShowPostcard(false);
-  }
+
 
   return (
     <div>
        <div className="SearchBarContainer">
             <SearchBar style={{ transform: 'scale(7) !important' }} />
           </div>
-          {showBackButton && ( 
-            <div className="SearchBarContainer">
-            <img className="backButton" src={Back} alt="Go Back" onClick={handleBackButton}/>
-            </div>
-            )}
-      {showAdviceTopContainer && (<div className="adviceTopContainer">
-        <AdviceInitialButton
-          type="Give Advice"
-          onClick={handleGiveAdviceClick}
-        />
-        <AdviceInitialButton
-          type="Ask For Advice"
-          onClick={handleAskAdviceClick}
-        />
-      <img className="imgA" src={GiveAdvice} alt="Give Advice" />
-     <img className="imgA" src={AskAdvice} alt="Ask For Advice" />
+          
+      <div className="centeredContainer threeRows">
+        <h3>Teaching without support is overwhelming</h3>
+        <h4 >Ask a teacher a question</h4>
+        <h4 >Answer a teacher's question</h4>
+     
 </div>
-      )}
-      {showAskAdvice && (
+    
         <>
          
-          
-          <div className="Titles">
-          <h2>Ask for Advice</h2>
-            <ActionButton skill="Ask Question" onClick={handleBtnAClick} />
-          </div>
-          <div className="AdviceInputContainer">
+          <div className="questionButtonContainer">
+          <img className="smImg" src={GiveAdvice} alt="Give Advice" />
+            {showAskQuestion && <ActionButton skill="Ask Question" onClick={handleBtnAClick} />}
             {showPostcard && <PostCard type="Question" />}
+            <img className="smImg" src={AskAdvice} alt="Ask Advice" />
           </div>
+          <h2 className="centeredContainer">Magic in progress..</h2>
+          
           <div className="receiveAdviceDiv">
             {askAdviceCardData.map((type, index) => (
             <ContentCard key={index} type={type.type} cardId={type.cardId} question={type.question} />
@@ -96,28 +63,7 @@ function Advice() {
          
           </div>
         </>
-      )}
-      {showGiveAdvice && (
-        <>
-          
-          
-          <div className="Titles">
-          <h2>Give Advice</h2>
-            <ActionButton skill="Add Advice" onClick={handleBtnAClick} />
-          </div>
-          <div className="AdviceInputContainer">
-            {showPostcard && <PostCard type="Advice" />}
-          </div>
-          <div className="giveAdviceDiv">
-            <ContentCard cardID="1" />
-            <ContentCard cardID="2" />
-            <ContentCard cardID="1" />
-            <ContentCard cardID="2" />
-            <ContentCard cardID="1" />
-            <ContentCard cardID="2" />
-          </div>
-        </>
-      )}
+      
       <div className="footerA">
       <Link to="/">
 <img src={Home} alt="Home" style={{ padding: "5px", }} />
