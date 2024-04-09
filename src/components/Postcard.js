07b589c7df3c7cd4f160fcd3c3ba141a86bfdcf9
Collette <garlandcollette@gmail.com>
2024-04-09 11:19:34 -0230

    fixed disappearing Question Button

--- src/components/Postcard.js
+++ src/components/Postcard.js
@@ -24,6 +24,7 @@ export default function Postcard({ type, cardId, setCommentsArray, setShowPostca
     setComment("");
     setCommentsArray((prevComments) => [newComment, ...prevComments]);
     setShowPostcard(false);
+    setShowAskQuestion(true);
   };
 
   return (
