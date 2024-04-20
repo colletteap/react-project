8ffdd2515c83f16633e56398d31123e14e98c070
Collette <garlandcollette@gmail.com>
2024-04-20 14:09:51 -0230

    fixed reorder icon for skilllanding on gridlanding

--- src/components/Skilllanding.js
+++ src/components/Skilllanding.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState} from "react";
+import { Grid } from "@mui/material";
+import ReorderIcon from "@mui/icons-material/Reorder";
+import useMediaQuery from '@mui/material/useMediaQuery';
 import "../styles/Skilllanding.css";
 import SkillButton from "./Skillbutton";
 import { skillData } from "./Data";
 
 const SkillLanding = () => {
+  const [openSkillLanding, setOpenSkillLanding] = useState(false);
+
+  const matches = useMediaQuery('(min-width:768px)');
+console.log("matches", matches, openSkillLanding);
+
+  const toggleSkillLanding = () => {
+    setOpenSkillLanding(!openSkillLanding);
+  };
+
   return (
     <>
       <h1 className="whiteHeading">Skills from real teachers</h1>
-      <div className="SkillContainer">
+      <Grid item xs={6} className="skillButton">
+      <button onClick={toggleSkillLanding}>
+          <ReorderIcon />
+        </button>
+        </Grid>
+      {(openSkillLanding || matches) && (<Grid>
         {skillData.map((skill, index) => (
           <SkillButton key={index} skill={skill.skill} linkTo={skill.linkTo} />
         ))}
-      </div>
+        <Grid item xs={6} className="skillButton">
+      <button onClick={toggleSkillLanding}>
+          <ReorderIcon />
+        </button>
+        </Grid>
+      </Grid>)}
+      
     </>
   );
 };
