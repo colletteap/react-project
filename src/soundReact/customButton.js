3afb134d6bd59b5bfc0f7ec8d6bf13e185cce11a
Collette <garlandcollette@gmail.com>
2024-04-28 20:15:43 -0230

    play sound custom button

--- src/soundReact/customButton.js
+++ src/soundReact/customButton.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Grid from "@mui/joy/Grid";
 import ClickSound from "./clicksound.wav";
 
 const CustomButton = () => {
     let audio = new Audio(ClickSound);
+
+    const buttonStyle = {
+        fontFamily: "Helvetica, sans serif",
+        border: "solid 1px #233349",
+        borderRadius: "15px",
+        backgroundColor: "white",
+        color: "#233349",
+        padding: "10px 20px",
+        cursor: "pointer"
+    };
 
     const start = () => {
         audio.play()
@@ -10,13 +21,9 @@ const CustomButton = () => {
     
 
     return (
-        <div>
-            <button onClick={start} 
-            sx={{
-                borderRadius: "15px",
-            }}
-            > Play Sound</button>
-        </div>
+        <Grid>
+            <button onClick={start} style={buttonStyle}> Play Sound</button>
+        </Grid>
     );
 };
 
