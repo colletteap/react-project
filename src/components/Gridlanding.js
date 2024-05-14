94541272a3169d2bdf9dd99621949274798d3c1b
Collette <garlandcollette@gmail.com>
2024-05-14 15:03:14 -0230

    making responsive design more friendly

--- src/components/Gridlanding.js
+++ src/components/Gridlanding.js
@@ -3,19 +3,29 @@ import Grid from "@mui/joy/Grid";
 import { Link } from "react-router-dom";
 import LargeLogo from "../assets/GrowthBlueHQ.png";
 import LogoText from "../assets/TopGridText.png";
+import GrowthSM from "../assets/GrowthSM.svg";
 import Emailimg from "../assets/Emailimg.png";
 import "../styles/Gridlanding.css";
 import SkillLanding from "./Skilllanding";
 import AdviceLanding from "./Advicelanding";
 import BlogLanding from "./Bloglanding";
-
+import useMediaQuery from "@mui/material/useMediaQuery"; 
 
 const GridLanding = () => {
+
+  const matches = useMediaQuery("(max-width:769px)")
+  const min = useMediaQuery("(min-width:769px)")
+
   return (
     <>
       <Grid className="GridContainer">
         <Grid className="ImageContainer">
+          {matches ? null :(
           <img src={LargeLogo} alt="Growth Logo" />
+          )}
+          {min ? null :(
+          <img src={GrowthSM} style={{paddingTop: "30px" }} alt="Growth" />
+          )}
         </Grid>
         <Grid className="ImageContainerText">
           <img src={LogoText} alt="LogoText" />
