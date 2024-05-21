7bccc129400c2d859b4e6a1d559dd5ee734152f5
Collette <garlandcollette@gmail.com>
2024-05-21 20:52:54 -0230

    working on pages

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route path= "/react-project/" element={<GridLanding />} />
-          <Route path= "/react-project/SkillShell/:skill" element={<SkillShell />} />
-          <Route path= "/react-project/SkillShell" element={<SkillShell />} />
-          <Route path= "/react-project/Advice" element={<Advice />} />
-          <Route path= "/react-project/Blog" element={<Blog />} />
-          <Route path= "/react-project//" element={<GridLanding />} />
+          <Route path= "SkillShell/:skill" element={<SkillShell />} />
+          <Route path= "SkillShell" element={<SkillShell />} />
+          <Route path= "Advice" element={<Advice />} />
+          <Route path= "Blog" element={<Blog />} />
+          <Route path= "/" element={<GridLanding />} />
         </Routes>
         <Footer/>
       </Router>
