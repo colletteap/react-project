d052e513acf87b5d0973f0069dc8dec1edc9b704
Collette <garlandcollette@gmail.com>
2024-05-22 09:36:01 -0230

    playing with pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Advice from "./pages/Advice";
 import Blog from "./pages/Blog";
 import GridLanding from "./components/Gridlanding";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -14,11 +14,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route path= "/react-project/" element={<GridLanding />} />
-          <Route path= "SkillShell/:skill" element={<SkillShell />} />
-          <Route path= "SkillShell" element={<SkillShell />} />
-          <Route path= "Advice" element={<Advice />} />
-          <Route path= "Blog" element={<Blog />} />
-          <Route path= "/" element={<GridLanding />} />
+          <Route path="/react-project/SkillShell/:skill" element={<SkillShell />} />
+          <Route path="/react-project/#/SkillShell" element={<SkillShell />} />
+          <Route path="/react-project/#/Advice" element={<Advice />} />
+          <Route path="/react-project/#/Blog" element={<Blog />} />
+          <Route path="/react-project/#/" element={<GridLanding />} />
         </Routes>
         <Footer/>
       </Router>
