2781e7b802e54ab0618c3a300a9ab255fb2a96ad
Collette <garlandcollette@gmail.com>
2024-06-05 16:27:11 -0230

    Revert "playing with pages"
    This reverts commit 9b89900e582209f16093f83e90ce069f1904d8e2.

--- src/App.js
+++ src/App.js
@@ -13,12 +13,21 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
+<<<<<<< HEAD
           <Route path="/" element={<GridLanding />} />
           <Route path="/SkillShell/:skill" element={<SkillShell />} />
           <Route path="/SkillShell" element={<SkillShell />} />
           <Route path="/Advice" element={<Advice />} />
           <Route path="/Blog" element={<Blog />} />
           <Route path="*" element={<GridLanding />} />
+=======
+          <Route path= "/react-project/" element={<GridLanding />} />
+          <Route path="/react-project/SkillShell/:skill" element={<SkillShell />} />
+          <Route path="/react-project/#/SkillShell" element={<SkillShell />} />
+          <Route path="/react-project/#/Advice" element={<Advice />} />
+          <Route path="/react-project/#/Blog" element={<Blog />} />
+          <Route path="/react-project/#/" element={<GridLanding />} />
+>>>>>>> parent of 9b89900e5 (playing with pages)
         </Routes>
         <Footer />
       </Router>
