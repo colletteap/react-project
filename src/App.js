e81872d62dd223ae5c4b878541635fc1666b7cca
Collette <garlandcollette@gmail.com>
2024-06-07 16:29:08 -0230

    project now working locally

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path= "/react-project/" element={<GridLanding />} />
-          <Route path="/react-project/SkillShell/:skill" element={<SkillShell />} />
-          <Route path="/react-project/#/SkillShell" element={<SkillShell />} />
-          <Route path="/react-project/#/Advice" element={<Advice />} />
-          <Route path="/react-project/#/Blog" element={<Blog />} />
-          <Route path="/react-project/#/" element={<GridLanding />} />
+          <Route path= "/" element={<GridLanding />} />
+          <Route path="/SkillShell/:skill" element={<SkillShell />} />
+          <Route path="/#/SkillShell" element={<SkillShell />} />
+          <Route path="/#/Advice" element={<Advice />} />
+          <Route path="/#/Blog" element={<Blog />} />
+          <Route path="/#/" element={<GridLanding />} />
         </Routes>
         <Footer />
       </Router>
